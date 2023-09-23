f953b75b7823866155393b5b299d53165696ae67
alexandrialin <71096478+alexandrialin@users.noreply.github.com>
2023-09-23 02:43:48 -0700

    Initial Commit

---
+++ transport-hack/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  transport-hack
+//
+//  Created by Aly Lin on 2023-09-23.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
