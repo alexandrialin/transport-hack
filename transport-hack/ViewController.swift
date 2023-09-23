56b7d0d56a9507ff577f3a066fd55030a3bfdca5
alexandrialin <71096478+alexandrialin@users.noreply.github.com>
2023-09-23 13:15:45 -0700

    photos and code

--- transport-hack/ViewController.swift
+++ transport-hack/ViewController.swift
@@ -1,12 +1,6 @@
-//
-//  ViewController.swift
-//  transport-hack
-//
-//  Created by Aly Lin on 2023-09-23.
-//
-
 import UIKit
 import RealityKit
+import ARKit
 
 class ViewController: UIViewController {
     
@@ -14,11 +8,41 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("running")
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        let configuration = ARImageTrackingConfiguration()
+
+        if let trackedImages = ARReferenceImage.referenceImages(inGroupNamed: "Your AR Resource Group Name", bundle: Bundle.main) {
+            configuration.trackingImages = trackedImages
+            configuration.maximumNumberOfTrackedImages = 2
+        }
         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        arView.session.run(configuration)
+        
+        arView.session.delegate = self
+    }
+}
+extension ViewController: ARSessionDelegate {
+    
+    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+        for anchor in anchors {
+            if let imageAnchor = anchor as? ARImageAnchor {
+                let referenceImageName = imageAnchor.referenceImage.name
+                
+                // Now handle the recognized image.
+                // For this example, let's just print the name of the recognized image:
+                print("Recognized image: \(referenceImageName ?? "Unknown")")
+                
+                // Here, you can add RealityKit entities or other actions based on the recognized image.
+                // Example: Display a text entity above the recognized image.
+                let textEntity = ModelEntity(mesh: .generateText(referenceImageName ?? "Unknown"))
+                textEntity.scale = [0.001, 0.001, 0.001] // scale down the text size for AR
+                arView.installGestures([.all], for: textEntity)
+                
+                let anchorEntity = AnchorEntity(anchor: imageAnchor)
+                anchorEntity.addChild(textEntity)
+                arView.scene.addAnchor(anchorEntity)
+            }
+        }
     }
 }
